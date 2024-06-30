55babe85384d035d80e0e12e46611bffd550daf1
samucaaaaaa <mucaspeedpvp@gmail.com>
2024-06-30 01:55:42 -0300

    Adicionando templates nos códigos

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 typedef enum { RED, BLACK } Color;
 
-typedef struct Node {
-    int data;
+template <typename T>
+struct Node {
+    T data;
     Color color;
-    struct Node *left, *right, *parent;
-} Node;
+    Node *left, *right, *parent;
+};
 
-Node* createNode(int data) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
+template <typename T>
+Node<T>* createNode(T data) {
+    Node<T>* newNode = (Node<T>*)malloc(sizeof(Node<T>));
     if (!newNode) {
         exit(1);
     }
@@ -20,8 +24,9 @@ Node* createNode(int data) {
     return newNode;
 }
 
-Node* rotateLeft(Node* root, Node* x) {
-    Node* y = x->right;
+template <typename T>
+Node<T>* rotateLeft(Node<T>* root, Node<T>* x) {
+    Node<T>* y = x->right;
     if (!y) return root; // Safety check
     x->right = y->left;
     if (y->left != nullptr) y->left->parent = x;
@@ -34,8 +39,9 @@ Node* rotateLeft(Node* root, Node* x) {
     return root;
 }
 
-Node* rotateRight(Node* root, Node* x) {
-    Node* y = x->left;
+template <typename T>
+Node<T>* rotateRight(Node<T>* root, Node<T>* x) {
+    Node<T>* y = x->left;
     if (!y) return root; // Safety check
     x->left = y->right;
     if (y->right != nullptr) y->right->parent = x;
@@ -48,10 +54,11 @@ Node* rotateRight(Node* root, Node* x) {
     return root;
 }
 
-Node* insertFixUp(Node* root, Node* z) {
+template <typename T>
+Node<T>* insertFixUp(Node<T>* root, Node<T>* z) {
     while (z->parent != nullptr && z->parent->color == RED) {
         if (z->parent == z->parent->parent->left) {
-            Node* y = z->parent->parent->right;
+            Node<T>* y = z->parent->parent->right;
             if (y != nullptr && y->color == RED) {
                 z->parent->color = BLACK;
                 y->color = BLACK;
@@ -67,7 +74,7 @@ Node* insertFixUp(Node* root, Node* z) {
                 root = rotateRight(root, z->parent->parent);
             }
         } else {
-            Node* y = z->parent->parent->left;
+            Node<T>* y = z->parent->parent->left;
             if (y != nullptr && y->color == RED) {
                 z->parent->color = BLACK;
                 y->color = BLACK;
@@ -88,9 +95,10 @@ Node* insertFixUp(Node* root, Node* z) {
     return root;
 }
 
-Node* insert(Node* root, Node* z) {
-    Node* y = nullptr;
-    Node* x = root;
+template <typename T>
+Node<T>* insert(Node<T>* root, Node<T>* z) {
+    Node<T>* y = nullptr;
+    Node<T>* x = root;
     while (x != nullptr) {
         y = x;
         if (z->data < x->data) x = x->left;
@@ -103,32 +111,37 @@ Node* insert(Node* root, Node* z) {
     return insertFixUp(root, z);
 }
 
-Node* minimum(Node* node) {
+template <typename T>
+Node<T>* minimum(Node<T>* node) {
     while (node->left != nullptr) node = node->left;
     return node;
 }
 
-Node* maximum(Node* node) {
+template <typename T>
+Node<T>* maximum(Node<T>* node) {
     while (node->right != nullptr) node = node->right;
     return node;
 }
 
-int height(Node* node) {
+template <typename T>
+int height(Node<T>* node) {
     if (node == nullptr) return 0;
     int leftHeight = height(node->left);
     int rightHeight = height(node->right);
     return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
 }
 
-void inorder(Node* root) {
+template <typename T>
+void inorder(Node<T>* root) {
     if (root != nullptr) {
         inorder(root->left);
-        printf("%d ", root->data);
+        cout << root->data << " ";
         inorder(root->right);
     }
 }
 
-Node* transplant(Node* root, Node* u, Node* v) {
+template <typename T>
+Node<T>* transplant(Node<T>* root, Node<T>* u, Node<T>* v) {
     if (u->parent == nullptr) root = v;
     else if (u == u->parent->left) u->parent->left = v;
     else u->parent->right = v;
@@ -136,10 +149,11 @@ Node* transplant(Node* root, Node* u, Node* v) {
     return root;
 }
 
-Node* deleteFixUp(Node* root, Node* x) {
+template <typename T>
+Node<T>* deleteFixUp(Node<T>* root, Node<T>* x) {
     while (x != root && (x == nullptr || x->color == BLACK)) {
         if (x != nullptr && x->parent != nullptr && x == x->parent->left) {
-            Node* w = x->parent->right;
+            Node<T>* w = x->parent->right;
             if (w != nullptr && w->color == RED) {
                 w->color = BLACK;
                 x->parent->color = RED;
@@ -153,7 +167,7 @@ Node* deleteFixUp(Node* root, Node* x) {
             } else {
                 if (w != nullptr && (w->right == nullptr || w->right->color == BLACK)) {
                     if (w->left != nullptr) w->left->color = BLACK;
-                    if (w != nullptr) w->color = RED;
+                    w->color = RED;
                     root = rotateRight(root, w);
                     w = x->parent->right;
                 }
@@ -164,7 +178,7 @@ Node* deleteFixUp(Node* root, Node* x) {
                 x = root;
             }
         } else {
-            Node* w = x != nullptr && x->parent != nullptr ? x->parent->left : nullptr;
+            Node<T>* w = x != nullptr && x->parent != nullptr ? x->parent->left : nullptr;
             if (w != nullptr && w->color == RED) {
                 w->color = BLACK;
                 if (x != nullptr && x->parent != nullptr) x->parent->color = RED;
@@ -194,9 +208,10 @@ Node* deleteFixUp(Node* root, Node* x) {
     return root;
 }
 
-Node* deleteNode(Node* root, Node* z) {
-    Node* y = z;
-    Node* x = nullptr;
+template <typename T>
+Node<T>* deleteNode(Node<T>* root, Node<T>* z) {
+    Node<T>* y = z;
+    Node<T>* x = nullptr;
     Color yOriginalColor = y->color;
     if (z->left == nullptr) {
         x = z->right;
@@ -225,7 +240,8 @@ Node* deleteNode(Node* root, Node* z) {
     return root;
 }
 
-Node* search(Node* root, int data) {
+template <typename T>
+Node<T>* search(Node<T>* root, T data) {
     while (root != nullptr && data != root->data) {
         if (data < root->data) root = root->left;
         else root = root->right;
@@ -233,7 +249,8 @@ Node* search(Node* root, int data) {
     return root;
 }
 
-int isRedBlackTree(Node* root, int blackCount, int* pathBlackCount) {
+template <typename T>
+int isRedBlackTree(Node<T>* root, int blackCount, int* pathBlackCount) {
     if (root == nullptr) {
         if (*pathBlackCount == -1) *pathBlackCount = blackCount;
         return blackCount == *pathBlackCount;
@@ -244,12 +261,14 @@ int isRedBlackTree(Node* root, int blackCount, int* pathBlackCount) {
     return isRedBlackTree(root->left, blackCount, pathBlackCount) && isRedBlackTree(root->right, blackCount, pathBlackCount);
 }
 
-int validateRedBlackTree(Node* root) {
+template <typename T>
+int validateRedBlackTree(Node<T>* root) {
     int pathBlackCount = -1;
     return root == nullptr || (root->color == BLACK && isRedBlackTree(root, 0, &pathBlackCount));
 }
 
-void freeTree(Node* root) {
+template <typename T>
+void freeTree(Node<T>* root) {
     if (root == nullptr) return;
     freeTree(root->left);
     freeTree(root->right);
@@ -257,7 +276,7 @@ void freeTree(Node* root) {
 }
 
 int main() {
-    Node* root = nullptr;
+    Node<int>* root = nullptr;
 
     root = insert(root, createNode(10));
     root = insert(root, createNode(20));
@@ -265,37 +284,37 @@ int main() {
     root = insert(root, createNode(15));
     root = insert(root, createNode(25));
 
-    printf("Inorder traversal: ");
+    cout << "Inorder traversal: ";
     inorder(root);
-    printf("\n");
+    cout << "\n";
 
     if (validateRedBlackTree(root))
-        printf("The tree is a valid red-black tree.\n");
+        cout << "The tree is a valid red-black tree.\n";
     else
-        printf("The tree is NOT a valid red-black tree.\n");
+        cout << "The tree is NOT a valid red-black tree.\n";
 
-    Node* found = search(root, 25);
+    Node<int>* found = search(root, 25);
     if (found != nullptr)
-        printf("Node with data 25 found.\n");
+        cout << "Node with data 25 found.\n";
     else
-        printf("Node with data 25 not found.\n");
+        cout << "Node with data 25 not found.\n";
 
     // Encontrar máximo e mínimo
-    Node* min = minimum(root);
-    Node* max = maximum(root);
-    printf("Minimum: %d\n", min->data);
-    printf("Maximum: %d\n", max->data);
+    Node<int>* min = minimum(root);
+    Node<int>* max = maximum(root);
+    cout << "Minimum: " << min->data << endl;
+    cout << "Maximum: " << max->data << endl;
 
     // Calcular altura
-    printf("Height: %d\n", height(root));
+    cout << "Height: " << height(root) << endl;
 
     // Remover um nó
-    Node* nodeToRemove = search(root, 15);
+    Node<int>* nodeToRemove = search(root, 15);
     if (nodeToRemove != nullptr)
         root = deleteNode(root, nodeToRemove);
-    printf("Inorder traversal after deletion: ");
+    cout << "Inorder traversal after deletion: ";
     inorder(root);
-    printf("\n");
+    cout << "\n";
 
     // Inserir mais nós para testes adicionais
     root = insert(root, createNode(5));
@@ -303,23 +322,23 @@ int main() {
     root = insert(root, createNode(40));
     root = insert(root, createNode(1));
 
-    printf("Inorder traversal after more insertions: ");
+    cout << "Inorder traversal after more insertions: ";
     inorder(root);
-    printf("\n");
+    cout << "\n";
 
     // Verificar  a árvore rubro-negra após inserções adicionais
     if (validateRedBlackTree(root))
-        printf("The tree is a valid red-black tree after more insertions.\n");
+        cout << "The tree is a valid red-black tree after more insertions.\n";
     else
-        printf("The tree is NOT a valid red-black tree after more insertions.\n");
+        cout << "The tree is NOT a valid red-black tree after more insertions.\n";
 
     // Remover nós adicionais
     nodeToRemove = search(root, 10);
     if (nodeToRemove != nullptr)
         root = deleteNode(root, nodeToRemove);
-    printf("Inorder traversal after deleting 10: ");
+    cout << "Inorder traversal after deleting 10: ";
     inorder(root);
-    printf("\n");
+    cout << "\n";
 
     // OBS: Ao inserir um número entre 20 e 29 o delete do 20 funciona
     root = insert(root, createNode(27));
@@ -328,9 +347,9 @@ int main() {
     nodeToRemove = search(root, 20);
     if (nodeToRemove != nullptr)
         root = deleteNode(root, nodeToRemove);
-    printf("Inorder traversal after deleting 20: ");
+    cout << "Inorder traversal after deleting 20: ";
     inorder(root);
-    printf("\n");
+    cout << "\n";
 
     freeTree(root);
     
